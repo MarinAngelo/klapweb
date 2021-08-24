ebebceba9fbb359c806abadc62f742271871885e
Angelo <angelo@klap-web.ch>
2021-08-24 17:26:04 +0200

    deleted ./src/stylesheets/main.scss in gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,8 +3,6 @@ import { PrismicPreviewProvider } from 'gatsby-plugin-prismic-previews'
 
 import 'gatsby-plugin-prismic-previews/dist/styles.css'
 
-import './src/stylesheets/main.scss'
-
 export const wrapRootElement = ({ element }) => (
   <PrismicPreviewProvider>{element}</PrismicPreviewProvider>
 )
