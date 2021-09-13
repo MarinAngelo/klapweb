0b59524d5ae2e625995a75e24c17a93fb7cc6821
Angelo <angelo@klap-web.ch>
2021-09-13 13:38:01 +0200

    ImageSlider on Homepage

--- src/slices/Carousel.js
+++ src/slices/Carousel.js
@@ -1,14 +1,37 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
-import { Card } from "react-bootstrap"
+import ImageSlider from 'react-bootstrap/Carousel'
 import styled from 'styled-components'
+import { RichText } from 'prismic-reactjs'
+
+const StyledImageSlider = styled(ImageSlider)`
+  margin-left: -15px;
+  margin-right: -15px;
+`
 
 export const Carousel = ({ slice }) => {
     console.log('Carousel data', slice)
 
     return (
         <div>
-            <h1>Carousel</h1>
+        {/* <SectionTitle title={slice.primary.section_title} /> */}
+        <StyledImageSlider>
+          {slice.items.map((slice, i) => (
+            <ImageSlider.Item key={i}>
+              <a href={slice.link}>
+                <img
+                  className="d-block w-100"
+                  src={slice.image.url}
+                  alt={slice.alt}
+                />
+              </a>
+              <ImageSlider.Caption>
+                <RichText render={slice.caption_title.raw} />
+                <RichText render={slice.caption_text.raw} />
+              </ImageSlider.Caption>
+            </ImageSlider.Item>
+          ))}
+        </StyledImageSlider>
         </div>
     )
 }
@@ -16,5 +39,25 @@ export const Carousel = ({ slice }) => {
 export const query = graphql`
 fragment PrismicHomepageDataBodyCarousel on PrismicHomepageDataBodyCarousel {
 slice_type
+items {
+              alt
+              caption_text {
+                raw
+              }
+              caption_title {
+                raw
+              }
+              image {
+                url
+              }
+              link {
+                url
+              }
+            }
+            primary {
+              section_title {
+                raw
+              }
+            }
 }
 `
