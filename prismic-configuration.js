b96fbb36ebc84c4bec7ad3f010d0a112af887cec
Angelo <angelo@klap-web.ch>
2022-01-06 18:50:49 +0100

    Initial config

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -7,11 +7,11 @@ module.exports = {
   // your repository.
   //
   // Example: 'my-repo' if your Prismic repository URL is 'my-repo.prismic.io'.
-  prismicRepo: 'klap-web-main',
+  prismicRepo: 'klap-bodywork',
 
   // The default language for content in your Prismic repository.
-  defaultLanguage: 'de-de',
+  defaultLanguage: 'de-ch',
 
   // All available languages for content in your Prismic repository.
-  langs: ['de-de']
+  langs: ['de-ch']
 }
