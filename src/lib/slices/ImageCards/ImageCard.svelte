a9dcf406af168c056624da13b6f2bee50906949d
Angelo <angelo@klap-web.ch>
2025-04-11 11:05:25 +0200

    Style in ImageCard from Store

--- src/lib/slices/ImageCards/ImageCard.svelte
+++ src/lib/slices/ImageCards/ImageCard.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import { isFilled, type Content } from '@prismicio/client';
 	import { PrismicImage, PrismicLink } from '@prismicio/svelte';
+	import { theme } from '$lib/stores/theme';
+	import { get } from 'svelte/store';
 
 	import PrismicRichText from '$lib/components/PrismicRichText.svelte';
 
 	export let card: Content.ImageCardsSliceDefaultPrimaryCardsItem;
+
+	const { pageBgColor } = get(theme);
 </script>
 
 <li class="grid gap-8">
 	{#if isFilled.image(card.image)}
-		<div class="bg-gray-100">
+		<div style="background-color: {pageBgColor};">
 			{#if isFilled.link(card.buttonLink)}
 				<PrismicLink field={card.buttonLink} tabindex={-1}>
 					<PrismicImage field={card.image} sizes="100vw" class="w-full" />
--- src/lib/slices/ImageCards/index.svelte
+++ src/lib/slices/ImageCards/index.svelte
@@ -2,20 +2,25 @@
 	import { isFilled, type Content } from '@prismicio/client';
 	import { PrismicText } from '@prismicio/svelte';
 
+	import { theme } from '$lib/stores/theme';
+	import { get } from 'svelte/store';
+
 	import Bounded from '$lib/components/Bounded.svelte';
 	import ImageCard from './ImageCard.svelte';
 	import Heading from '$lib/components/Heading.svelte';
 
 	export let slice: Content.ImageCardsSlice;
+
+	const { pageBgColor, pageColor } = get(theme);
 </script>
 
 <Bounded
 	tag="section"
-	class="bg-white"
+	style="background-color: {pageBgColor};"
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
 >
-	<div class="grid gap-12">
+	<div class="grid gap-12" style="color: {pageColor}">
 		{#if isFilled.richText(slice.primary.heading)}
 			<Heading class="text-center">
 				<PrismicText field={slice.primary.heading} />
