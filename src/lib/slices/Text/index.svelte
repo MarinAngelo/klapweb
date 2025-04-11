c581e2f560158737f5960eb564b055222f9f2549
Angelo <angelo@klap-web.ch>
2025-04-11 11:17:44 +0200

    Styles in Text from Store

--- src/lib/slices/Text/index.svelte
+++ src/lib/slices/Text/index.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client';
 	import clsx from 'clsx';
+	import { theme } from '$lib/stores/theme';
+	import { get } from 'svelte/store';
 
 	import Bounded from '$lib/components/Bounded.svelte';
 	import PrismicRichText from '$lib/components/PrismicRichText.svelte';
 
 	export let slice: Content.TextSlice;
+
+	const { pageColor, pageBgColor } = get(theme);
 </script>
 
 <Bounded
 	as="section"
-	class="bg-white leading-relaxed"
+	class="leading-relaxed"
+	style="color: {pageColor}; background-color: {pageBgColor};"
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
 >
