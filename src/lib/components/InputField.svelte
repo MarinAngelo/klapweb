b261c56c4c001ca313dfe300fa281c9ea5e3fc2d
Angelo <angelo@klap-web.ch>
2025-04-17 12:13:56 +0200

    formular style

--- src/lib/components/InputField.svelte
+++ src/lib/components/InputField.svelte
@@ -1,34 +1,42 @@
 <script lang="ts">
-    export let field: {
-        field_name: string | null;
-        field_type: string;
-        required: boolean;
-        'invalid_feedback-text'?: string | null;
-    };
+	import { theme } from '$lib/stores/theme';
+	import { get } from 'svelte/store';
+	export let field: {
+		field_name: string | null;
+		field_type: string;
+		required: boolean;
+		'invalid_feedback-text'?: string | null;
+	};
 </script>
 
 <div class="mb-4">
-    <label for={field.field_name} class="block text-sm font-medium">
-        {field.field_name}
-    </label>
-    {#if field.field_type === 'text' || field.field_type === 'email'}
-        <input
-            type={field.field_type}
-            id={field.field_name}
-            name={field.field_name}
-            required={field.required}
-            class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-        />
-    {:else if field.field_type === 'textarea'}
-        <textarea
-            id={field.field_name}
-            name={field.field_name}
-            required={field.required}
-            rows="4"
-            class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-        ></textarea>
-    {/if}
-    {#if field['invalid_feedback-text']}
-        <p class="text-red-500 text-sm mt-1">{field['invalid_feedback-text']}</p>
-    {/if}
+	<label for={field.field_name} class="block text-sm font-medium">
+		{field.field_name}
+	</label>
+	{#if field.field_type === 'text' || field.field_type === 'email'}
+		<input
+			type={field.field_type}
+			id={field.field_name}
+			name={field.field_name}
+			required={field.required}
+			class="mt-1 p-2 block w-full rounded-none border-b-2 focus:outline-none focus:ring-0 sm:text-sm"
+			style="background-color: {get(theme).pageBgColor}; color: {get(theme)
+				.pageColor}; border-bottom-color: {get(theme).pageColor};"
+		/>
+	{:else if field.field_type === 'textarea'}
+    <div class="border-b-2" style="border-bottom-color: {get(theme).pageColor};">
+		<textarea
+			id={field.field_name}
+			name={field.field_name}
+			required={field.required}
+			rows="4"
+			class="mt-1 p-2 block w-full rounded-md sm:text-sm"
+            			style="background-color: {get(theme).pageBgColor}; color: {get(theme)
+				.pageColor};"
+		></textarea>
+    </div>
+	{/if}
+	{#if field['invalid_feedback-text']}
+		<p class="text-red-500 text-sm mt-1">{field['invalid_feedback-text']}</p>
+	{/if}
 </div>
