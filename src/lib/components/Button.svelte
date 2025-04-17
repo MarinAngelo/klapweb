3a61bae2edd3aa22d8eba4477f85863ceadba382
Angelo <angelo@klap-web.ch>
2025-04-17 11:51:56 +0200

    Button kleiner

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -9,9 +9,9 @@
 
 <PrismicLink
 	field={link}
-	class="inline-block px-4 py-2 font-semibold rounded-full hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+	class="text-xs sm:text-sm inline-block px-4 py-2 font-semibold rounded-full hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 	style="--button-bg-color: {get(theme).pageBgColor}; --button-text-color: {get(theme)
-		.pageColor}; border: 2px solid var(--button-text-color);"
+		.pageColor}; border: 1px solid var(--button-text-color);"
 >
 	{text}
 </PrismicLink>
