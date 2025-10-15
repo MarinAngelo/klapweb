26d0196b93193a73043d4c0a0ff70c351a196983
Angelo <angelo@klap-web.ch>
2025-10-15 21:49:15 +0200

    Funktioniert jetzt auch in der Desktop Ansicht perfekt!

--- src/lib/slices/Kacheln/ImageCard.svelte
+++ src/lib/slices/Kacheln/ImageCard.svelte
@@ -41,17 +41,16 @@
 		</div>
 	{/if}
 	<div
-		class="leading-relaxed {bodyBgColor || borderColor ? 'pl-6 pr-6' : ''}"
-		class:pl-6={isMobile}
-		class:pr-6={isMobile}
+		class="leading-relaxed"
+		class:pl-6={$isMobile || !!bodyBgColor || !!borderColor}
+		class:pr-6={$isMobile || !!bodyBgColor || !!borderColor}
 	>
 		<PrismicRichText field={card.text} />
 	</div>
 	{#if isFilled.link(card.buttonLink)}
 		<div
-			class={bodyBgColor || (borderColor && isMobile) ? 'pl-6' : ''}
-			class:pl-6={isMobile}
-			class:pr-6={isMobile}
+			class:pl-6={$isMobile || !!bodyBgColor || !!borderColor}
+			class:pr-6={$isMobile || !!bodyBgColor || !!borderColor}
 		>
 			<Button
 				link={card.buttonLink}
