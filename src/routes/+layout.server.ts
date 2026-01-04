8c065871edbd8917535cb387653cba25e1e2a982
marinAngelo <angelo@klap.ch>
2026-01-04 18:39:39 +0100

    /home/angelo/web/klapweb/src/routes/+layout.server.ts zurückgesetzt

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,16 +3,21 @@ import { createClient } from '$lib/prismicio';
 export const prerender = false;
 
 export async function load(event) {
+  event.setHeaders({
+    'cache-control': 'no-store'
+  });
+
   const client = createClient({ fetch: event.fetch, cookies: event.cookies });
 
-  // Passe die Single-Types an, falls sie bei dir anders heißen:
   const settings = await client.getSingle('settings');
   const navigation = await client.getSingle('navigation');
-  const theme = await client.getSingle('theme');
+  const prismicTheme = await client.getSingle('theme');
+  const fonts = await client.getAllByType('font');
 
   return {
     settings,
     navigation,
-    theme
+    prismicTheme,
+    fonts
   };
 }
