1cff838357f2d71a4785b4dbe837bc5af691d25a
marinAngelo <angelo@klap.ch>
2026-02-02 15:56:27 +0100

    Korrekte Darstellung in allen Iterationen

--- src/lib/components/EmbedFieldHandler.svelte
+++ src/lib/components/EmbedFieldHandler.svelte
@@ -3,20 +3,18 @@
 	import { onMount } from 'svelte';
 	export let embed: any;
 
-	// Nach dem Rendern: width/height-Attribute des iframes entfernen/überschreiben
+	// Nach dem Rendern: width/height-Attribute aller iframes entfernen/überschreiben
 	onMount(() => {
-		const container = document.querySelector('.media-embed-html');
-		if (container) {
-			const iframe = container.querySelector('iframe');
-			if (iframe) {
-				iframe.removeAttribute('width');
-				iframe.removeAttribute('height');
-				iframe.style.width = '100%';
-				iframe.style.aspectRatio = '16/9';
-				iframe.style.height = 'auto';
-				iframe.style.minHeight = '200px';
-			}
-		}
+		const iframes = document.querySelectorAll('.media-embed-html iframe');
+		iframes.forEach((iframe) => {
+			const el = iframe as HTMLIFrameElement;
+			el.removeAttribute('width');
+			el.removeAttribute('height');
+			el.style.width = '100%';
+			el.style.aspectRatio = '16/9';
+			el.style.height = 'auto';
+			el.style.minHeight = '200px';
+		});
 	});
 </script>
 
