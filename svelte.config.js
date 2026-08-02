f6611a9414d075fb94a0ba48ca70751338cb1b38
marinAngelo <angelo@klap.ch>
2026-08-02 17:40:47 +0200

    netlify error

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,8 +6,6 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto ist super für Vercel/Netlify.
-		// Für einen VPS (Ubuntu) würdest du später evtl. auf @sveltejs/adapter-node wechseln.
 		adapter: adapter(),
 
 		prerender: {
